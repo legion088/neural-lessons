6562d2f68e810ed79938334740c9ef6fbe4e4760
Ilnar Shamionov <ilnar_08@mail.ru>
2023-03-22 08:26:52 +0400

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def activation(x):
+    return 0 if x < 0.5 else 1
+
+
+def choice(house, music, beauty):
+    inp_signals = np.array(([house, music, beauty]))
+    weight1 = np.array([
+        [0.3, 0.3, 0],
+        [0.4, -0.5, 1]
+    ])
+    weight2 = np.array([-1, 1])
+
+    sum_hidden = np.dot(weight1, inp_signals)
+    out_hidden = np.array([activation(x) for x in sum_hidden])
+    sum_end = np.dot(weight2, out_hidden)
+
+    return activation(sum_end)
+
+
+if __name__ == '__main__':
+    print(choice(0, 0.5, 0.5))
